ad8dc6992d229ebf98b6d2587374a095ac794164
Johannes Wachter <johannes.wachter@massiveart.at>
2015-11-07 23:12:37 +0100

    fixed namespace

--- src/DependencyInjection/WorkerCompilerPass.php
+++ src/DependencyInjection/WorkerCompilerPass.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DependencyInjection;
+namespace Task\TaskBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
